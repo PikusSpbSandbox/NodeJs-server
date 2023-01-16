fb3a308ceab707da99e03774ba4c546f4e31c539
Petr Savelev <petr.savelev.direct@gmail.com>
2023-01-16 20:16:33 +0300

    Added MySQL support; minor changes

--- web-server.js
+++ web-server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const https = require('https');
 const cors = require('cors');
-const fs = require('fs');
+const mysql = require("mysql2");
 
 const port = 8181;
 const retryCount = 5;
-const historyFile = __dirname + '/data-history.json';
+
+const DB_CONFIG = Object.freeze({
+  connectionLimit: 5,
+  host: "localhost",
+  user: "root",
+  database: "usersdb2",
+  password: "123456"
+});
 
 class WebServer {
   retried = 0;
-  url = 'http://';
+  queryApiUrl = 'http://';
 
   request(url) {
     return new Promise((resolve, reject) => {
@@ -48,30 +55,43 @@ class WebServer {
   }
   makeRequest() {
     this.retried = 0;
-    return this.retry(() => this.request(this.url), retryCount).then(data => this.handleRequest(data));
+    return this.retry(() => this.request(this.queryApiUrl), retryCount);
   }
 
-  handleRequest(data = []) {
-    const dataHistory = JSON.parse(fs.readFileSync(historyFile, 'utf8'));
+  async handleRequest(data = []) {
+    const pool = mysql.createPool(DB_CONFIG);
     const now = new Date();
     const month = String(now.getMonth() + 1).padStart(2, '0');
     const date = String(now.getDate()).padStart(2, '0');
     const todayString = `${now.getFullYear()}.${month}.${date}`;
 
-    /* sever history data logic here */
-    /* ...  */
-    /* eo sever history data logic here */
-
-
-    const content = JSON.stringify(dataHistory);
-
-    fs.writeFile(historyFile, content, err => {
-      if (err) {
-        console.error(err);
-      }
+    // Query history from DB
+    const historyData = await new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM history', (err, result) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        }
+        resolve(result);
+      });
     });
 
-    return dataHistory;
+    /* `historyData` handling logic here */
+    /* ...  */
+    /* eo `historyData` handling logic here */
+
+    // Insert new data to DB
+    await new Promise((resolve, reject) => {
+      pool.query("INSERT INTO history (date, value) VALUES (?,?)", [todayString, `data.value`], (err, inserted) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        }
+        resolve(inserted)
+      });
+    });
+
+    return historyData;
   }
 }
 
@@ -83,7 +103,7 @@ expressApp.listen(port, () => {
   console.log(`Web server is listening on port ${port}`);
 });
 expressApp.get('/', (req, res) => {
-  myServer.makeRequest().then(response => {
-    res.json( response );
+  myServer.makeRequest().then(response => myServer.handleRequest(response)).then(data => {
+    res.json(data);
   });
 });
